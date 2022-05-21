a2ecc35a4aa5386e203e058322fc91f0207d4baa
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2022-05-21 00:35:56 -0300

    material 21/maio

---
+++ servidor-monolitinho/Doc.md
@@ -0,0 +1,24 @@
+## Documentação
+
+### GHIBLI
+- [GET] "/ghibli" - retornar todos os filmes
+
+- [GET] "/ghibli/buscar/:id" - retorna o filme com id selecionado
+
+- [GET] "/ghibli/filtro{nome}" - retorna o filme com nome selecionado
+
+- [POST]"/ghibli/cadastrar" - cadastra um novo filme
+
+### FILMES
+- [GET] "/filmes" - retornar todos os filmes
+
+- [GET] "/filmes/buscar/:id" - retorna o filme com id selecionado
+
+- [POST]"/filmes/cadastrar" - cadastra um novo filme
+
+### SERIES
+- [GET] "/filmes" - retornar todos os filmes
+
+- [GET] "/filmes/buscar/:id" - retorna o filme com id selecionado
+
+- [POST]"/filmes/cadastrar" - cadastra um novo filme
---
+++ servidor-monolitinho/server.js
@@ -0,0 +1,65 @@
+const ghibliJson = require("./data/ghibli.json")
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (request, response)=>{
+    response.status(200).json([
+        {
+            "mensagem":"API de filmes Ghibli"
+        }
+    ])
+})
+
+
+app.get("/ghibli/filmes", (request, response)=>{
+    response.status(200).send(ghibliJson)
+})
+
+
+
+app.get("/ghibli/buscar/:id", (request, response)=>{
+    let idRequest = request.params.id
+    let filmeEncontrado = ghibliJson.find(filme => filme.id == idRequest)
+
+    response.status(200).send(filmeEncontrado)
+
+})
+
+app.get("/ghibli/filtro", (request, response)=>{
+    let tituloRequest = request.query.titulo.toLowerCase()
+
+    let filmeEncontrado = ghibliJson.filter(
+        filme => filme.title.toLowerCase().includes(tituloRequest))
+
+    response.status(200).send(filmeEncontrado)
+})
+
+
+app.post("/ghibli/cadastrar", (request,response)=>{
+    let bodyRequest = request.body
+
+    let novoFilme = {
+        id: (ghibliJson.length)+1, 
+        title: bodyRequest.title, 
+        description: bodyRequest.description 
+    }
+    ghibliJson.push(novoFilme)
+    
+    response.status(201).send({
+        "mensagem": "filmes cadastrado com sucesso",
+        novoFilme
+    })
+})
+
+
+
+app.listen(3030, ()=>{
+    console.log("alô, pepe moreno? to na porta 3030")
+})
