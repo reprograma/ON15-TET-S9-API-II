2f08abb1b3a3a4b8df28adc7cde0c49402dcd112
BrunaCelestino <brunaccelestino@gmail.com>
2022-05-21 12:09:44 -0300

    Aula 21/05

--- servidor-monolitinho/server.js
+++ servidor-monolitinho/server.js
@@ -9,22 +9,22 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get("/", (request, response)=>{
+app.get("/", (request, response) => {
     response.status(200).json([
         {
-            "mensagem":"API de filmes Ghibli"
+            "mensagem": "API de filmes Ghibli"
         }
     ])
 })
 
 
-app.get("/ghibli/filmes", (request, response)=>{
+app.get("/ghibli/filmes", (request, response) => {
     response.status(200).send(ghibliJson)
 })
 
 
 
-app.get("/ghibli/buscar/:id", (request, response)=>{
+app.get("/ghibli/buscar/:id", (request, response) => {
     let idRequest = request.params.id
     let filmeEncontrado = ghibliJson.find(filme => filme.id == idRequest)
 
@@ -32,7 +32,7 @@ app.get("/ghibli/buscar/:id", (request, response)=>{
 
 })
 
-app.get("/ghibli/filtro", (request, response)=>{
+app.get("/ghibli/filtro", (request, response) => {
     let tituloRequest = request.query.titulo.toLowerCase()
 
     let filmeEncontrado = ghibliJson.filter(
@@ -42,24 +42,71 @@ app.get("/ghibli/filtro", (request, response)=>{
 })
 
 
-app.post("/ghibli/cadastrar", (request,response)=>{
+app.post("/ghibli/cadastrar", (request, response) => {
     let bodyRequest = request.body
 
     let novoFilme = {
-        id: (ghibliJson.length)+1, 
-        title: bodyRequest.title, 
-        description: bodyRequest.description 
+        id: (ghibliJson.length) + 1,
+        title: bodyRequest.title,
+        description: bodyRequest.description
     }
     ghibliJson.push(novoFilme)
-    
+
     response.status(201).send({
         "mensagem": "filmes cadastrado com sucesso",
         novoFilme
     })
 })
 
+app.delete("/ghibli/deletar/:id", (request, response) => {
+    const idRequest = request.params.id
+    const filmeEncontrado = ghibliJson.find(filme => filme.id == idRequest)
 
+    const indice = ghibliJson.indexOf(filmeEncontrado)
+    ghibliJson.splice(indice, 1)
 
-app.listen(3030, ()=>{
+    response.status(200).json([{
+        "mensagem": "filme deletado com sucesso",
+        "filme-deletado": filmeEncontrado,
+        ghibliJson
+    }])
+})
+
+app.put("/ghibli/substituir/:id", (request, response) => {
+    const idRequest = request.params.id
+    const bodyRequest = request.body
+
+    const filmeEncontrado = ghibliJson.find(filme => filme.id == idRequest)
+
+    const indice = ghibliJson.indexOf(filmeEncontrado)
+
+    bodyRequest.id = idRequest
+
+    ghibliJson.splice(indice, 1, bodyRequest)
+
+    response.status(200).json([{
+        "mensagem": "filme atualizado com sucesso",
+        "filme-atualizado": bodyRequest,
+        ghibliJson
+    }])
+})
+
+app.patch("/ghibli/updateTitulo/:id", (request, response) => {
+    const idRequest = request.params.id
+    const newTitle = request.body.title
+
+    const filmeEncontrado = ghibliJson.find(filme => filme.id == idRequest)
+
+    filmeEncontrado.title = newTitle
+
+    response.status(200).json([{
+        "mensagem": "titulo atualizado com sucesso",
+        "filme-atualizado": filmeEncontrado,
+        ghibliJson
+    }])
+
+})
+
+app.listen(3030, () => {
     console.log("alô, pepe moreno? to na porta 3030")
 })
