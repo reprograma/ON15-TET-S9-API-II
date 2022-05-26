db4ef717fb7028331bc0c2db81a24ae6c1c29368
gabskovisk <gabskovisk@gmail.com>
2022-05-26 19:45:09 -0300

    corrigir erro de sintaxe

--- paraolar/src/app.js
+++ paraolar/src/app.js
@@ -1,4 +1,17 @@
 const express = require("express")
+const cors = require("cors")
+
 const app = express() 
 
 app.use(express.json())
+
+app.use(cors())
+
+const filmesRoutes = require("./routes/filmesRoutes")
+
+
+app.use("/filmes", filmesRoutes)
+
+
+
+module.exports = app
---
+++ paraolar/src/routes/filmesRoutes.js
@@ -0,0 +1,25 @@
+const controller = require("../controllers/filmesController")
+
+const express = require("express")
+
+const router = express.Router()
+
+router.get("/catalogo", controller.getAll) 
+
+router.get("/buscar/:id", controller.getById)
+
+router.get("/filtrar", controller.getByTitle)
+
+router.get("/genero", controller.getByGenre)
+
+router.post("/cadastrar", controller.createMovie)
+
+router.put("/substituir/:id", controller.replaceTitle)
+
+router.patch("/updateTitulo/:id", controller.updateTitle)
+
+// router.patch("/update/:id", controller.updateYear)
+
+router.delete("/deletar/:id", controller.deleteTitle)
+
+module.exports = router
